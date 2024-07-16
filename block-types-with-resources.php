1acf583c99eee47ce3355bf1cb1eaa4650c508d3
David Calhoun <github@davidcalhoun.me>
2024-07-16 12:51:56 -0400

    feat: Rudimentary resources via `rest_prepare_block_type` hook
    Hook the existing block types endpoint rather than creating a new
    endpoint.

--- block-types-with-resources.php
+++ block-types-with-resources.php
@@ -3,11 +3,44 @@
  * Plugin Name: Block Types with Resources
  */
 
-require_once plugin_dir_path(__FILE__) . "includes/controller.php";
+function btr_map_block_type_with_resources($response, $block_type, $request) {
+    global $wp_styles;
+    global $wp_scripts;
 
-function btr_register_rest_routes() {
-    $controller = new BTR_REST_Block_Types_Controller();
-    $controller->register_routes();
+    foreach ($block_type as $key => $value) {
+        if (
+            strpos($key, "script") !== false &&
+            substr($key, -8) === "_handles"
+        ) {
+            $resource_key = substr($key, 0, -8);
+            $resource_paths = [];
+            foreach ($value as $handle) {
+                if (isset($wp_styles->registered[$handle])) {
+                    $resource_paths[] = $wp_scripts->registered[$handle]->src;
+                }
+            }
+            $response->data[$resource_key . "_paths"] = $resource_paths;
+        } elseif (
+            strpos($key, "style") !== false &&
+            substr($key, -8) === "_handles"
+        ) {
+            $resource_key = substr($key, 0, -8);
+            $resource_paths = [];
+            foreach ($value as $handle) {
+                if (isset($wp_styles->registered[$handle])) {
+                    $resource_paths[] = $wp_styles->registered[$handle]->src;
+                }
+            }
+            $response->data[$resource_key . "_paths"] = $resource_paths;
+        }
+    }
+
+    return $response;
 }
 
-add_action("rest_api_init", "btr_register_rest_routes");
+add_action(
+    "rest_prepare_block_type",
+    "btr_map_block_type_with_resources",
+    10,
+    3,
+);
